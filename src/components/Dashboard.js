896b0138597cf69227b10082519a64ae919dcbc3
Ellen Skipper <ellen.skipper@btinternet.com>
2019-05-12 00:19:00 +0100

    Can sort of recall a drone

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import IncidentPinpoint from './IncidentPinpoint';
 import DronePinpoint from './DronePinpoint';
 import shouldPureComponentUpdate from 'react-pure-render/function';
+import $ from "jquery";
 
 const controlPanelStyle = {height: '100%', width: '30%', lineHeight: '17px', padding: '15px'}
 export default class Dashboard extends Component {
@@ -65,9 +66,108 @@ export default class Dashboard extends Component {
   }
 
   recallDrone(id) {
+    const drones = this.state.drones;
+    const recalledDrone = drones.find(drone => drone.id === id);
     console.log('Recall Drone '+id)
-  }
+    console.log(JSON.stringify({ drone_id: recalledDrone.id,
+      d_lon: recalledDrone.current_pos._longitude,
+      d_lat: recalledDrone.current_pos._latitude,
+      event_id: recalledDrone.event_id,
+      speed: recalledDrone.speed,
+      capacity: recalledDrone.capacity,
+      isRecall: true }));
+    //     const urlWithId = `https://us-central1-firedrones-19.cloudfunctions.net/changeDrones`;
+    //     const bodyData = {
+    //   "drone_id" : "ErJRu5Pa0B9vOE9RA104",
+    //   "d_lon": 50.12,
+    //   "d_lat": -2.1234,
+    //   "event_id" : "" ,
+    //   "speed": 20,
+    //   "capacity": 80,
+    //   "isRecall" : true
+    // }
+    //     fetch(urlWithId, {
+    //         method: 'PUT',
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify(bodyData)
+    //     })
+    //     .then(response => response.json());
+    // var http = require("https");
 
+    // var options = {
+    //   "method": "PUT",
+    //   "hostname": [
+    //     "https://cors-anywhere.herokuapp.com/us-central1-firedrones-19",
+    //     "cloudfunctions",
+    //     "net"
+    //   ],
+    //   "path": [
+    //     "changeDrones"
+    //   ],
+    //   "headers": {
+    //     "Content-Type": "application/json",
+    //     "User-Agent": "PostmanRuntime/7.11.0",
+    //     "Accept": "/",
+    //     "Cache-Control": "no-cache",
+    //     "Postman-Token": "49270a63-4145-4c7a-b645-0fc58daf31fb,039d4ee0-2afe-498a-8262-51d9865e4029",
+    //     "Host": "cors-anywhere.herokuapp.com/us-central1-firedrones-19.cloudfunctions.net",
+    //     "accept-encoding": "gzip, deflate",
+    //     "content-length": "146",
+    //     "Connection": "keep-alive",
+    //     "cache-control": "no-cache"
+    //   }
+    // };
+
+    // var req = http.request(options, function (res) {
+    //   var chunks = [];
+
+    //   res.on("data", function (chunk) {
+    //     chunks.push(chunk);
+    //   });
+
+    //   res.on("end", function () {
+    //     var body = Buffer.concat(chunks);
+    //     console.log(body.toString());
+    //   });
+    // });
+
+    // req.write(JSON.stringify({ drone_id: recalledDrone.id,
+    //   d_lon: recalledDrone.current_pos._longitude,
+    //   d_lat: recalledDrone.current_pos._latitude,
+    //   event_id: recalledDrone.event_id,
+    //   speed: recalledDrone.speed,
+    //   capacity: recalledDrone.capacity,
+    //   isRecall: true }));
+    // req.end();
+    //   }
+    const insertData = "{\n\t\"drone_id\" : \"" +recalledDrone.id+"\",\n\t\"d_lon\":"+recalledDrone.current_pos._longitude+",\n\t\"d_lat\": "+recalledDrone.current_pos._latitude+",\n\t\"event_id\" : \""+recalledDrone.event_id+"\" ,\n\t\"speed\": "+recalledDrone.speed+",\n\t\"capacity\": "+recalledDrone.capacity+",\n\t\"isRecall\" : true\n}"
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://us-central1-firedrones-19.cloudfunctions.net/changeDrones",
+      "method": "PUT",
+      "headers": {
+        "Content-Type": "application/json",
+        "User-Agent": "PostmanRuntime/7.11.0",
+        "Accept": "/",
+        "Cache-Control": "no-cache",
+        "Postman-Token": "3875e849-0131-4a1b-96d6-d90fe068f216,26cb8b72-4f33-4f51-87cf-d2c46de03975",
+        "Host": "us-central1-firedrones-19.cloudfunctions.net",
+        "accept-encoding": "gzip, deflate",
+        "content-length": "146",
+        "Connection": "keep-alive",
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": insertData
+    }
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  }
   deleteIncident(id) {
     console.log('Delete Incident '+id)
   }
@@ -140,6 +240,9 @@ export default class Dashboard extends Component {
           <br/>
           <b>Speed:</b> <br/> 
           {selectedDroneInfo && selectedDroneInfo.speed}
+          <br/>
+          <b>Recalled:</b> <br/> 
+          {selectedDroneInfo.Recall ? 'Yes' : 'No'}
           <br/>
         <b>Assigned to incident:</b><br/> 
           {selectedDroneInfo.event_id ? selectedDroneInfo.event_id : 'Currently unassigned'}
