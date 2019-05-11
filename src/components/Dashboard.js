4053960b236d8fa345cfb7a1df1072ed0385ed9d
Ellen Skipper <ellen.skipper@btinternet.com>
2019-05-11 22:15:09 +0100

    Add drone pinpoints, menu and functions for select drone or incident

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
-import MapPinpoint from './MapPinpoint';
+import IncidentPinpoint from './IncidentPinpoint';
+import DronePinpoint from './DronePinpoint';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 
+const controlPanelStyle = {height: '100%', width: '30%', lineHeight: '17px', padding: '15px'}
 export default class Dashboard extends Component {
 
   constructor(props) {
     super(props);
     this.setIncident = this.setIncident.bind(this);
+    this.setDrone = this.setDrone.bind(this);
+    this.recallDrone = this.recallDrone.bind(this);
+    this.deleteIncident = this.deleteIncident.bind(this);
+    this.changeSeverity = this.changeSeverity.bind(this);
+
     this.state = {
       incidents: [],
       drones: [],
       isIncidentsLoaded: false,
       isDronesLoaded: false,
       selectedIncident: '',
+      selectedDrone: '',
     }
   }
 
@@ -49,7 +57,23 @@ export default class Dashboard extends Component {
   shouldComponentUpdate = shouldPureComponentUpdate;
 
   setIncident(id) {
-    this.setState({selectedIncident: id});
+    this.setState({selectedIncident: id, selectedDrone: ''});
+  }
+
+  setDrone(id) {
+    this.setState({selectedIncident: '', selectedDrone: id});
+  }
+
+  recallDrone(id) {
+    console.log('Recall Drone '+id)
+  }
+
+  deleteIncident(id) {
+    console.log('Delete Incident '+id)
+  }
+
+  changeSeverity(id, severity) {
+    console.log('Set severity of incident '+id+' to '+severity)
   }
 
   render() {
@@ -68,6 +92,71 @@ export default class Dashboard extends Component {
     else{
       const selectedIncidentId = this.state.selectedIncident;
       const selectedIncidentInfo = incidents.find(incident => incident.id === selectedIncidentId);
+      const selectedDroneId = this.state.selectedDrone;
+      const selectedDroneInfo = drones.find(drone => drone.id === selectedDroneId);
+      const assignedDrone = drones.find(drone => drone.event_id === selectedIncidentId);
+
+      var controlPanel;
+      if (selectedIncidentId) {
+        controlPanel =
+        <div style={controlPanelStyle}>
+          <b>Selected Incident:</b><br/>
+          {selectedIncidentInfo && selectedIncidentInfo.id} 
+        <br/>
+          <b>Description:</b> <br/>
+          {selectedIncidentInfo && selectedIncidentInfo.description} 
+        <br/>
+          <b>Assigned drones:</b> <br/>
+          {assignedDrone ? assignedDrone : 'Currently none assigned'}
+          <button onClick={() => this.deleteIncident(selectedIncidentId)}>
+            Delete Incident
+          </button>
+          Set incident severity: 
+          <button onClick={() => this.changeSeverity(selectedIncidentId, 1)}>
+            1
+          </button> 
+          <button onClick={() => this.changeSeverity(selectedIncidentId, 2)}>
+            2
+          </button> 
+          <button onClick={() => this.changeSeverity(selectedIncidentId, 3)}>
+            3
+          </button> 
+          <button onClick={() => this.changeSeverity(selectedIncidentId, 4)}>
+            4
+          </button> 
+          <button onClick={() => this.changeSeverity(selectedIncidentId, 5)}>
+            5
+          </button> 
+        </div>
+      }
+      else if (selectedDroneId) {
+        controlPanel =
+        <div style={controlPanelStyle}>
+          <b>Selected Drone:</b><br/>
+          {selectedDroneInfo && selectedDroneInfo.id} 
+          <br/>
+          <b>Capacity:</b> <br/> 
+          {selectedDroneInfo && selectedDroneInfo.capacity}
+          <br/>
+          <b>Speed:</b> <br/> 
+          {selectedDroneInfo && selectedDroneInfo.speed}
+          <br/>
+        <b>Assigned to incident:</b><br/> 
+          {selectedDroneInfo.event_id ? selectedDroneInfo.event_id : 'Currently unassigned'}
+          <button onClick={() => this.recallDrone(selectedDroneId)}>
+            Recall Drone
+          </button>
+        </div>
+      }
+      else {
+        controlPanel =
+        <div style={controlPanelStyle}>
+        <p>
+          <b>Select an incident or a drone for options.</b>
+        </p>
+        </div>
+      }
+
       return (
         //Lets hack from here?
         //https://github.com/google-map-react/old-examples/blob/master/web/flux/components/examples/x_main/main_map_block.jsx
@@ -84,7 +173,7 @@ export default class Dashboard extends Component {
                   >
                   {incidents.map(incident => {
                     return (
-                      <MapPinpoint
+                      <IncidentPinpoint
                         lat={incident.location._latitude}
                         lng={incident.location._longitude}
                         text={incident.severity}
@@ -95,17 +184,19 @@ export default class Dashboard extends Component {
                       />
                     )
                   })}
+                  {drones.map(drone => {
+                    return (
+                      <DronePinpoint
+                        lat={drone.current_pos._latitude}
+                        lng={drone.current_pos._longitude}
+                        key={drone.id}
+                        eventId={drone.event_id}
+                        setDrone={() => this.setDrone(drone.id)}
+                      />
+                    )
+                  })}
                   </GoogleMapReact> 
-                <div style={{height: '100%', width: '30%', background: 'blue'}}>
-                  <p>
-                    Selected Incident:
-                    {selectedIncidentInfo && ' '+selectedIncidentInfo.id} 
-                  </p>
-                  <p>
-                    Description: 
-                    {selectedIncidentInfo && ' '+selectedIncidentInfo.description}
-                  </p>
-                </div>
+                {controlPanel}
               </div>
           </div>
         </section>
--- src/components/DronePinpoint.js
+++ src/components/DronePinpoint.js
@@ -3,23 +3,23 @@ import shouldPureComponentUpdate from 'react-pure-render/function';
 
 const pinpointStyle = {
     color: 'white',
-    background: '#e25a09',
-    padding: '6px 9px',
+    background: 'blue',
+    padding: '6px 6px',
     display: 'inline-flex',
     textAlign: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: '100%',
     transform: 'translate(-50%, -50%)',
     zIndex: 1,
     position: 'relative',
+    width: '10px',
+    height: '10px'
   }
 
-const imgStyle = {maxWidth: '150px', maxHeight: '130px'}
 
-const popupStyle = {background: '#e25a09', position: 'absolute', transform: 'translate(0px, -110px)', height: '164px', padding: '5px', borderRadius: '7px', zIndex: 99  }
+// const popupStyle = {background: '#e25a09', position: 'absolute', transform: 'translate(0px, -110px)', height: '164px', padding: '5px', borderRadius: '7px', zIndex: 99  }
 
-export default class MyGreatPlaceWithHover extends Component {
+export default class IncidentPinpoint extends Component {
     static defaultProps = {};
 
     //To rerender when on/off hover
@@ -30,15 +30,15 @@ export default class MyGreatPlaceWithHover extends Component {
   }
 
   render() {
-    const popupHoverStyle = this.props.$hover ? popupStyle : {display:'none', zIndex: 99  };
+    // const popupHoverStyle = this.props.$hover ? popupStyle : {display:'none', zIndex: 99  };
     const divHoverStyle = this.props.$hover ? {...pinpointStyle, zIndex: 99} : pinpointStyle;
     return (
-       <div style={divHoverStyle} onClick={this.props.setIncident}>
+       <div style={divHoverStyle} onClick={this.props.setDrone}>
           {this.props.text}
-          <div className='imgcontainer' style={popupHoverStyle}>
+          {/* <div className='imgcontainer' style={popupHoverStyle}>
           <img style={imgStyle} className="mapImage" src={'data:image/png;base64,'.concat(this.props.imgcode)} alt=''/>
           {this.props.description}
-          </div>
+          </div> */}
        </div>
     );
   }
